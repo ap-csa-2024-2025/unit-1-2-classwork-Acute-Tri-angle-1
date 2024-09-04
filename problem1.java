f74a8579367ca86d1c245951d29335b9d3e54019
Acute-Tri-angle <pptran106@student.hbuhsd.edu>
2024-09-04 10:51:15 -0700

    go go power rangers

--- problem1.java
+++ problem1.java
@@ -1,1 +1,7 @@
 // Create your class here; don't forget the main method
+public class problem1{
+    public static void main(String[] args){
+        String favFood = "eggrolls";
+        System.out.println("I like to eat " + favFood + " as well!");
+    }
+}
--- problem2.java
+++ problem2.java
@@ -1,1 +1,12 @@
 // Create your class here; don't forget the main method
+public class problem2{
+    public static void main(String[] args){
+        String name1 = "Jeff ";
+        String name2 = "Joey ";
+        String name3 = "Joe ";
+        String name4 = "Jacob ";
+        
+        System.out.println(name4 + name2 + name3 + name1);
+
+    }
+}
